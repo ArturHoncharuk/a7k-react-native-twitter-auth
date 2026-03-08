bc815095b608688de4d337d6eef8a8d867d61de0
ArturHoncharuk <dotwork.play@gmail.com>
2026-03-08 20:14:41 +0200

    chore: remove unused APP_SCHEME_SLUG constant from index.ts

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,5 +1,3 @@
-export const APP_SCHEME_SLUG = 'foobar';
-
 export const TWITTER_API_URLS = {
   xRequestTokenUrl: 'https://api.x.com/oauth/request_token',
   xAccessTokenUrl: 'https://api.x.com/oauth/access_token',
